952a8fd782af2b4fd0daecd5951646dfc0dc06ee
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-15 23:18:23 +0200

    add tast about global scope

--- lesson-02/index.js
+++ lesson-02/index.js
@@ -220,3 +220,56 @@
 // 1. Перевіряє yci case/default чи відповідає якийсь виразу switch.
 // 2. Якщо знаходить такий кейс - починає виконувати до break в switch або до кінця циклу,
 // 3. Якщо знаходить такий кейс - переходить до наступної інструкції після switch
+
+
+// Задача про область видимості
+
+const global = "global";
+
+if (true) {
+  const blockA = "block A";
+
+  // Бачимо глобальну + локальну A
+  console.log(global); // 'global'
+  console.log(blockA); // block A
+
+  // Змінні blockB і blockC не знайдені в доступних областях видимості.
+  // Буде помилка звернення до змінної.
+  console.log(blockB); // ReferenceError: blockB is not defined
+  console.log(blockC); // ReferenceError: blockC is not defined
+
+  if (true) {
+    const blockB = "block B";
+
+    // Бачимо глобальну + зовнішню A + локальну B
+    console.log(global); // global
+    console.log(blockA); // block A
+    console.log(blockB); // block B
+
+    // Змінна blockC не знайдена в доступних областях видимості.
+    // Буде помилка звернення до змінної.
+    console.log(blockC); // ReferenceError: blockC is not defined
+  }
+}
+
+if (true) {
+  const blockC = "block C";
+
+  // Бачимо глобальну + локальну C
+  console.log(global); // global
+  console.log(blockC); // block C
+
+  // Змінні blockA і blockB не знайдені в доступних областях видимості.
+  // Буде помилка звернення до змінної.
+  console.log(blockA); // ReferenceError: blockA is not defined
+  console.log(blockB); // ReferenceError: blockB is not defined
+}
+
+// Бачимо лише глобальну
+console.log(global); // global
+
+// Змінні blockA, blockB і blockC не знайдені в доступних областях видимості.
+// Буде помилка звернення до змінної.
+console.log(blockA); // ReferenceError: blockA is not defined
+console.log(blockB); // ReferenceError: blockB is not defined
+console.log(blockC); // ReferenceError: blockC is not defined
