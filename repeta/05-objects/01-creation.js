485bf1b1487395b36e89401cc3c859ace56d5a8a
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-23 15:48:41 +0200

    update objects creation

--- repeta/05-objects/01-creation.js
+++ repeta/05-objects/01-creation.js
@@ -155,25 +155,143 @@
 // console.log(secondTitle); // Сон смішної людини
 // console.log(secondCoverImage); // https://via.placeholder.com/640/480
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "На березі спокійних вод",
-    author: "Роберт Шеклі",
-    rating: 8.51,
-  },
-];
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "На березі спокійних вод",
+//     author: "Роберт Шеклі",
+//     rating: 8.51,
+//   },
+// ];
 
-for (const book of books) {
-  console.log(book.title);
-  console.log(book.author);
-  console.log(book.rating);
-}
+// for (const book of books) {
+//   console.log(book.title);
+//   console.log(book.author);
+//   console.log(book.rating);
+// }
 
 // Об'єкти
 
+// Объекты(делаем плейлист музыки: имя, рейтинг, треки, кол - во треков)
+// - Литерал объекта
+// - Свойства, ключи(имя) и значения
+// - Как отличить объект от области ВИДИМОСТИ
 
+// const playlist = {
+//   name: 'My super playlist',
+//   rating: 5,
+//   tracks: ['track-1', 'track-2', 'track-3'],
+//   trackCount: 3,
+// };
+
+// console.log(playlist);
+
+// const x = {};
+
+// const fn = function (myObject) {
+//   // myObject = { a: 1, b: 2 }
+//   console.log(myObject);
+// };
+
+// fn({ a: 1, b: 2 });
+
+// const rtfm = function () {
+//   return { a: 5 };
+// };
+
+// console.log(rtfm);
+
+
+// Доступ K свойству
+// - obj.key
+// - obj[`key']
+// - Отсутствующие свойства
+
+// const playlist = {
+//   name: 'My super playlist',
+//   rating: 5,
+//   tracks: ['track-1', 'track-2', 'track-3'],
+//   trackCount: 3,
+// };
+
+// Добавити властивість
+// playlist.qwe = 5;
+
+// console.log(playlist);
+// console.log(playlist.tracks);
+// console.log(playlist.name);
+// console.log(playlist.trackCount);
+
+// const propertyName = 'tracks';
+
+// console.log(playlist.rating);
+// console.log(playlist['rating']);
+
+// console.log(playlist.propertyName);
+// console.log(playlist[propertyName]);
+
+
+
+// Короткая запись свойств
+
+// const username = 'Mango';
+// const email = 'mango@mail.com';
+
+// Довгий запис
+// const signupData = {
+//   username: username,
+//   email: email,
+// };
+
+// Короткий запис
+// const signupData = {
+//   username,
+//   email,
+// };
+
+// console.log(signupData);
+
+// Вычисляемые свойства
+
+// <input name="color" value="tomato" >
+
+// const inputName = 'color';
+// const inputValue = 'tomato';
+
+// const colorPickerData = {
+//   [inputName]: inputValue,
+// };
+
+// console.log(colorPickerData);
+
+// Ссылочный тип {}==={}
+
+// console.log({ a: 1 } === { a: 1 });
+// console.log([] === []);
+
+// const a = { x: 1, y: 2 };
+// const b = a;
+
+// console.log(b === a);
+
+// a.hello = 100;
+
+// console.log(a);
+// console.log(b);
+
+// Массивы и Функции это ОБЬЕКТЫ
+
+// const a = [1, 2, 3];
+// a.hello = '^)';
+// console.log(a);
+
+// const fn = function () {
+//   console.log('hello');
+// }
+
+// fn.hello = '$)';
+// console.dir(fn.hello);
