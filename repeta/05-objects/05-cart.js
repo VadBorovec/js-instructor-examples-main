49bbb305ef8a13923717f91fa5dff64f226a211e
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-02-25 00:53:46 +0200

    done 6 lesson objects

--- repeta/05-objects/05-cart.js
+++ repeta/05-objects/05-cart.js
@@ -14,29 +14,70 @@
 
 const cart = {
     items: [],
-    getItems() { },
-    add(product) { },
-    remove(productName) { },
-    clear() { },
-    countTotalPrice() { },
+    getItems() {
+        return this.items;
+    },
+    add(product) {
+        for (const item of this.items) {
+            if (item.name === product.name) {
+                item.quantity += 1;
+                return;
+            }
+        }
+
+        const newProduct = {
+            ...product,
+            quantity: 1,
+        };
+        this.items.push(newProduct);
+    },
+    remove(productName) {
+        const { items } = this;
+        for (let i = 0; i < items.length; i += 1) {
+            const { name} = items[i];
+            if (productName === name) {
+                console.log('find this product: ', productName);
+                items.splice(i, 1);
+            }
+        }
+    },
+    countTotalPrice() {
+        let total = 0;
+        const { items } = this;
+        for (const { price, quantity } of items) {
+            total += price * quantity;
+        }
+        return total;
+    },
+    clear() {
+        this.items = [];
+    },
     increaseQuantity(productName) { },
     decreaseQuantity(productName) { },
 };
 
-// console.table(cart.getItems());
+console.log(cart.getItems());
 
 cart.add({ name: 'apple', price: 50 });
+cart.add({ name: 'grape', price: 60 });
 cart.add({ name: 'lemon', price: 60 });
 cart.add({ name: 'lemon', price: 60 });
 cart.add({ name: 'strawbrry', price: 110 });
+cart.add({ name: 'strawbrry', price: 110 });
+cart.add({ name: 'strawbrry', price: 110 });
 
-// console.table(cart.getItems());
 
-// cart.remove('apple');
-// console.table(cart.getItems());
+console.table(cart.getItems());
 
-// cart.clear();
-// console.table(cart.getItems());
+cart.remove('apple');
+console.table(cart.getItems());
+
+console.log('TOTAL: ', cart.countTotalPrice());
+
+cart.clear();
+console.log(cart.getItems());
+
+console.log('TOTAL: ', cart.countTotalPrice());
 
 // cart.increaseQuantity('apple');
 // console.table(cart.getItems());
