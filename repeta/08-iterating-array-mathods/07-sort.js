e7ad203222ca61532479877121b2a5e28a940246
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-03 23:57:25 +0200

    prepare file sort.js

---
+++ repeta/08-iterating-array-mathods/07-sort.js
@@ -0,0 +1,34 @@
+// Array.prototype.sort(callback(currentEl, nextEl){})
+// - Сортирует и ИЗМЕНЯЕТ оригинальный массив
+// - По умолчанию приводит элементы K строке и сортирует по юникоду
+
+const numbers = [1, 9, 6, 2, 3];
+
+const letters = ['b', 'B', 'a', 'A'];
+// letters.sort();
+// console.log(letters);
+
+// Как сделать копию массива чтобы не сортировать оригинальный
+
+// const copy = numbers.slice();
+// const copy = [...numbers];
+// console.log(numbers);
+// console.log(copy);
+
+// Кастомная сортировка сложных типов
+
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+  { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
+];
+
+// По игровому времени
+
+// const sortedByTopPlayers = [...players].sort();
+// console.table(sortedByTopPtayers);
+
+// const sortedByWorstPlayers = [...players].sort();
+// console.table(sortedByWorstPlayers);
