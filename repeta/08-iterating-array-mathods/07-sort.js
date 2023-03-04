ad37f1d1637e08600adcb3e1f5ec098f356379a4
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-04 20:51:49 +0200

    done sort.js / add flat.js, lodash.js

--- repeta/08-iterating-array-mathods/07-sort.js
+++ repeta/08-iterating-array-mathods/07-sort.js
@@ -1,19 +1,49 @@
 // Array.prototype.sort(callback(currentEl, nextEl){})
 // - Сортирует и ИЗМЕНЯЕТ оригинальный массив
-// - По умолчанию приводит элементы K строке и сортирует по юникоду
+// - По умолчанию приводит элементы K строке и сортирует по !юникоду!
 
 const numbers = [1, 9, 6, 2, 3];
+// numbers.sort();
+// console.log(numbers);
 
 const letters = ['b', 'B', 'a', 'A'];
 // letters.sort();
 // console.log(letters);
 
-// Как сделать копию массива чтобы не сортировать оригинальный
+// compareFunction - функция сравнения (callback)
+// Элементы массива сортируются В соответствии C её возвращаемым значением
+// - если compareFunction(A, B) меньше 0, сортировка поставит A перед В
+// - если compareFunction(A, B) больше 0, сортировка поставит В перед A
+// - если compareFunction(A, B) вернёт 0, сортировка оставит A и В на
+// неизменными по отношению друг K другу, но отсортирует их по отношению KO всем
+// другим элементам,
 
-// const copy = numbers.slice();
-// const copy = [...numbers];
+// // По возрастанию
+// numbers.sort((curEl, nextEl) => {
+//   return curEl - nextEl;
+// });
+
+// // По убыванию
+// numbers.sort((curEl, nextEl) => {
+//   return nextEl - curEl;
+// });
+
 // console.log(numbers);
-// console.log(copy);
+
+// Как сделать копию массива чтобы не сортировать оригинальный
+// - Array.prototype.slice()
+// - Operation spread
+
+const copySortUp = [...numbers];
+copySortUp.sort();
+const copySortDown = [...numbers].sort((a, b) => b - a);
+
+// console.log('numbers', numbers);
+// console.log('copySortUp', copySortUp);
+// console.log('copySortDown', copySortDown);
+
+// OR
+// console.log([1, 2, 3, 4, 5].reverse());
 
 // Кастомная сортировка сложных типов
 
@@ -24,11 +54,29 @@ const players = [
   { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
   { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
 ];
+// console.table(players);
 
 // По игровому времени
 
-// const sortedByTopPlayers = [...players].sort();
-// console.table(sortedByTopPtayers);
+const sortedByTopPlayers = [...players].sort(
+  (prevPlayer, nextPlayer) => nextPlayer.timePlayed - prevPlayer.timePlayed
+);
+// console.table(sortedByTopPlayers);
 
-// const sortedByWorstPlayers = [...players].sort();
+const sortedByWorstPlayers = [...players].sort(
+  (prevPlayer, nextPlayer) => prevPlayer.timePlayed - nextPlayer.timePlayed
+);
 // console.table(sortedByWorstPlayers);
+
+// Sort by NAME
+
+const sortedByName = [...players].sort((prevPlayer, nextPlayer) => {
+  const result = prevPlayer.name[0] > nextPlayer.name[0];
+
+  if (result) {
+    return 1;
+  }
+  return -1;
+});
+
+// console.table(sortedByName);
