d9c885ea58f26fe67455cb9f4d249b03859f548d
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-09 01:11:14 +0200

    update 09-lesson js file

--- weekdays/lesson-09/index.js
+++ weekdays/lesson-09/index.js
@@ -6,7 +6,7 @@
 // розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та
 // кількістю з властивості `stones`.
 
-// ```js
+// // ```js
 // const chopShop = {
 //   stones: [
 //     { name: 'Emerald', price: 1300, quantity: 4 },
@@ -14,30 +14,40 @@
 //     { name: 'Sapphire', price: 1400, quantity: 7 },
 //     { name: 'Ruby', price: 800, quantity: 2 },
 //   ],
-//   calcTotalPrice(stoneName) {},
+//   calcTotalPrice(stoneName) {
+//     const currentStone = this.stones.find(({ name }) => stoneName === name);
+//     return currentStone.price * currentStone.quantity;
+//   },
 // };
 
 // console.log(chopShop.calcTotalPrice('Emerald')); // 5200
 // console.log(chopShop.calcTotalPrice('Diamond')); // 8100
 // console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
 // console.log(chopShop.calcTotalPrice('Ruby')); // 1600
-// ```
+// // ```
+
+// 1. Контекст у функції визначається на момент її виклику,
+// а не на момент створення
+// 2. Контекстом функції буде обєкт який її викликав
+
+// //============================================================
 
 // ## Example 2 - Телефонна книга
 
 // Виконайте рефакторинг методів об'єкту `phonebook` щоб код запрацював.
 
-// ```js
+// // ```js
 // const phonebook = {
 //   contacts: [],
 //   add(contact) {
 //     const newContact = {
 //       list: 'default',
 //       ...contact,
-//       id: generateId(),
-//       createdAt: getDate(),
+//       id: this.generateId(),
+//       createdAt: this.getDate(),
 //     };
-//     contacts.push(newContact);
+//     this.contacts.push(newContact);
+//     return this.contacts;
 //   },
 //   generateId() {
 //     return '_' + Math.random().toString(36).substr(2, 9);
@@ -47,20 +57,22 @@
 //   },
 // };
 
-// console.log(
+// console.table(
 //   phonebook.add({
 //     name: 'Mango',
 //     email: 'mango@mail.com',
 //     list: 'friends',
-//   }),
+//   })
 // );
-// console.log(
+// console.table(
 //   phonebook.add({
 //     name: 'Poly',
 //     email: 'poly@hotmail.com',
-//   }),
+//   })
 // );
-// ```
+// // ```
+
+// //============================================================
 
 // ## Example 3 - Калькулятор
 
@@ -70,6 +82,88 @@
 // - `add()` - повертає суму збережених значень.
 // - `mult()` - перемножує збережені значення та повертає результат.
 
-// ```js
-// const calculator = {};
-// ```
+// // ```js
+// const calculator = {
+//   a: 0,
+//   b: 0,
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+
+//     return this;
+//   },
+//   add() {
+//     return this.a + this.b;
+//   },
+//   mult() {
+//     return this.a * this.b;
+//   },
+// };
+
+// calculator.read(5, 10);
+// console.log(calculator);
+// console.log(calculator.add()); // 15
+// console.log(calculator.mult()); // 50
+// console.log(calculator.read(11, 3).mult()); // 33
+// // ```
+
+// //======================================================
+// this in callback functions
+
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     console.log(this);
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() - ue виклик методу getFullName без об'єкта
+//   console.log(`06робляемо заявку від ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName.bind(customer));
+// // makeMessage(
+// //   customer.getFullName.bind({
+// //     firstName: 'Olg',
+// //     lastName: 'Bond',
+// //   })
+// // );
+
+// call, аррly - викликають ф-цію- на місці
+// bind - робить копію - ф-ції
+
+// 'use strict';
+
+// function globalFunc() {
+//   console.log(this);
+// }
+
+// const globalArrowFunc = () => {
+//   console.log(this);
+// };
+
+// console.log(this); // ?
+// globalFunc(); // ?
+// globalArrowFunc(); // ? must be Window , but there we have - undefined
+
+//! при передачі ф-ції в якості колбеку, ii контекст не зберігається
+//! для прив язки контексту використувуються методи call, apply i bind
+//! call/apply викликають ф-цію на місці i вказують ій контекст (1 аргумент)
+//! bind - - 3B' 'язує, робить копію ф-ції i вказує контекст
+// (використовуйте при передачі ф-ції В якості колбеку)
+
+// dsfsdf
+// sdfsdf
+// dfsdf
+// sdfsdf
+// /fsdfsdf
+// sdfsdfs
+// /sdfsdfs
+// casas
+// caaca
+// cadcsdcsd
+// cvsdvsdcdasc
+// cdscdsc
