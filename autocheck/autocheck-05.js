878d6f4d681323b49db77e007b372e88636208ac
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-11 23:56:42 +0200

    done all autocheck tasks in 5 module

--- autocheck/autocheck-05.js
+++ autocheck/autocheck-05.js
@@ -474,15 +474,534 @@
 // Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
 // Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
 
-// Change code above this line
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+// class StringBuilder {
+//   constructor(initialValue) {
+//     this.value = initialValue;
+//   }
+//   getValue() {
+//     return this.value;
+//   }
+//   padEnd(str) {
+//     this.value += str;
+//   }
+//   padStart(str) {
+//     this.value = str + this.value;
+//   }
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // "."
+// builder.padStart('^');
+// console.log(builder.getValue()); // "^."
+// builder.padEnd('^');
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth('=');
+// console.log(builder.getValue()); // "=^.^="
 
 // //===============================================================
 // 12
+
+// Виконай рефакторинг класу Car таким чином, щоб властивість brand була приватною,
+// і додай два методи для публічного інтерфейсу, для читання і зміни цієї властивості.
+
+// getBrand() - повертає значення приватної властивості brand.
+//     changeBrand(newBrand) - змінює значення приватної властивості brand на newBrand.
+
+// Оголошений клас Car
+// Властивість brand в класі Car оголошена приватною
+// Конструктор класу приймає об'єкт з властивостями brand, model і price
+// В результаті виклику new Car({ brand: "Audi", model: "Q3", price: 36000 }) утвориться об'єкт
+// { model: "Q3", price: 36000 }
+// В результаті виклику new Car({ brand: "bmw", model: "X5", price: 58900 }) утвориться об'єкт
+// { model: "X5", price: 58900 }
+// В результаті виклику new Car({ brand: "Nissan", model: "Murano", price: 31700 }) утвориться об'єкт
+// { model: "Murano", price: 31700 }
+// В екземпляра відсутня публічна властивість brand
+// Метод getBrand() повертає значення приватної властивості brand.
+// Метод changeBrand("Honda") змінює значення приватної властивості brand на "Honda"
+
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   // Change code above this line
+// }
+
+// const car1 = new Car({ brand: 'Audi', model: 'Q3', price: 36000 });
+// console.log(car1);
+// const car2 = new Car({ brand: 'bmw', model: 'X5', price: 58900 });
+// console.log(car2);
+// const car3 = new Car({ brand: 'Nissan', model: 'Murano', price: 31700 });
+// console.log(car3);
+
+// console.log(car1.getBrand());
+// console.log(car2.getBrand());
+// console.log(car3.getBrand());
+
+// car3.changeBrand('Honda');
+// console.log(car3.getBrand());
+
+// //===============================================================
+// 13 WAREHOUSE 2.0 / СКЛАД 2.0
+
+// Виконай рефакторинг класу Storage, зробивши властивість items приватною.
+
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності,
+// в якій твій код перевірятимуть тести.Будь ласка, нічого там не змінюй.
+
+// Оголошений клас Storage
+// Об'єкт storage не містить властивості items
+// В класі Storage оголошений метод getItems
+// В класі Storage оголошений метод addItem
+// В класі Storage оголошений метод removeItem
+// Властивість items в класі Storage оголошена приватною
+// Конструктор класу приймає властивість items
+// В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"])
+// значення змінної storage - це об'єкт
+// Перший виклик storage.getItems(), відразу після ініціалізації екземпляра, повертає масив
+// ["Nanitoids", "Prolonger", "Antigravitator"]
+// Другий виклик, storage.getItems(), після виклику storage.addItem("Droid"), повертає масив
+// ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// Третій виклик storage.getItems(), після виклику storage.removeItem("Prolonger"), повертає масив
+// ["Nanitoids", "Antigravitator", "Droid"]
+
+// class Storage {
+//   // Change code below this line
+//   #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem('Droid');
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+// //===============================================================
+// 14 STRING CONSTRUCTOR 2.0 / КОНСТРУКТОР РЯДКІВ 2.0
+
+// Виконай рефакторинг класу StringBuilder, зробивши властивість value приватною.
+
+// Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності,
+// в якій твій код перевірятимуть тести.Будь ласка, нічого там не змінюй.
+
+// Оголошений клас StringBuilder
+// Властивість value в класі StringBuilder оголошена приватною
+// В класі StringBuilder оголошений метод getValue
+// В класі StringBuilder оголошений метод padEnd
+// В класі StringBuilder оголошений метод padStart
+// В класі StringBuilder оголошений метод padBoth
+// В результаті виклику new StringBuilder('.') значення змінної builder - це об'єкт
+// Об'єкт builder не містить властивості value
+// Перший виклик builder.getValue(), відразу після ініціалізації екземпляра, повертає рядок .
+// Другий виклик builder.getValue(), після виклику builder.padStart("^"), повертає рядок ^.
+// Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
+// Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
+
+// class StringBuilder {
+//   // Change code below this line
+//   #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // "."
+// builder.padStart('^');
+// console.log(builder.getValue()); // "^."
+// builder.padEnd('^');
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth('=');
+// console.log(builder.getValue()); // "=^.^="
+
+// //===============================================================
+// 15
+
+// Виконай рефакторинг класу Car.Зроби властивості model і price приватними, а також #brand.
+// Стандартизуй публічний інтерфейс класу, замінивши вже оголошені методи на гетери та сетери
+// brand, model і price, для взаємодії з приватними властивостями.
+
+// Оголошений клас Car
+// В класі Car оголошена приватна властивість brand
+// В класі Car оголошена приватна властивість model
+// В класі Car оголошена приватна властивість price
+// Конструктор класу приймає об'єкт з властивостями brand, model і price
+// В класі Car оголошений гетер brand
+// В класі Car оголошений сетер brand
+// В класі Car оголошений гетер model
+// В класі Car оголошений сетер model
+// В класі Car оголошений гетер price
+// В класі Car оголошений сетер price
+
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   #model;
+//   #price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
+// const car1 = new Car({ brand: 'Audi', model: 'Q3', price: 36000 });
+// console.log(car1);
+// const car2 = new Car({ brand: 'bmw', model: 'X5', price: 58900 });
+// console.log(car2);
+// const car3 = new Car({ brand: 'Nissan', model: 'Murano', price: 31700 });
+// console.log(car3);
+
+// console.log(car1.brand);
+// console.log(car2.brand);
+// console.log(car3.brand);
+
+// car3.brand = 'Honda';
+// console.log(car3.brand);
+
+// //===============================================================
+// 16
+
+// Виконай рефакторинг класу Car.Додай публічну статичну властивість MAX_PRICE
+// зі значенням 50000 - максимально допустима ціна автомобіля.
+
+// Додай сетеру price перевірку значення параметра newPrice, що передається.Якщо воно більше
+// за MAX_PRICE, сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну автомобіля.
+
+// Оголошений клас Car
+// Клас Car містить статичну властивість MAX_PRICE
+// Значення статичної властивості MAX_PRICE - це число 50000
+// Екземпляр не містить властивості MAX_PRICE
+// В класі Car оголошений гетер price
+// В класі Car оголошений сетер price
+// Виклик сетера price в екземпляра класу, зі значенням аргументу меншим за значення MAX_PRICE,
+//     змінює властивість #price
+// Виклик сетера price в екземпляра класу, зі значенням аргументу більшим за значення MAX_PRICE,
+//     не змінює властивість #price
+
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice > Car.MAX_PRICE) {
+//       return this.price;
+//     }
+//     this.#price = newPrice;
+//   }
+
+//   //   set price(newPrice) {
+//   //     if (newPrice <= Car.MAX_PRICE) {
+//   //       this.#price = newPrice;
+//   //     }
+//   //   }
+
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// //===============================================================
+// 17
+
+// Додай класу Car публічний статичний метод checkPrice(price), що приймає ціну автомобіля.
+// Метод повинен порівняти значення параметра price і приватної статичного властивості MAX_PRICE.
+
+// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum".
+// В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits".
+// Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів, щоб показати,
+//     як буде використовуватися метод checkPrice(price).
+
+// Оголошений клас Car
+// Клас Car містить статичний метод checkPrice(price)
+// Виклик Car.checkPrice(36000) повертає рядок "Success! Price is within acceptable limits"
+// Виклик Car.checkPrice(18000) повертає рядок "Success! Price is within acceptable limits"
+// Виклик Car.checkPrice(64000) повертає рядок "Error! Price exceeds the maximum"
+// Виклик Car.checkPrice(57000) повертає рядок "Error! Price exceeds the maximum"
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       return 'Error! Price exceeds the maximum';
+//     }
+//     return 'Success! Price is within acceptable limits';
+//   }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// //===============================================================
+// 18
+
+// У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
+
+// Оголоси клас Admin, який наслідує від класу User
+// Додай класу Admin публічну статичну властивість AccessLevel(рівень доступу), значення якої — це об'єкт
+// { BASIC: "basic", SUPERUSER: "superuser" }
+// Оголошений клас Admin
+// Клас Admin наслідує від класу User
+// Клас Admin містить публічну статичну властивість AccessLevel
+// Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+// Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Change code below this line
+
+// class Admin extends User {
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+// }
+
+// console.log(Admin.AccessLevel.BASIC);
+// console.log(Admin.AccessLevel.SUPERUSER);
+
+// //===============================================================
+// 19
+
+// Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з двома властивостями
+// email і accessLevel.Додай класу Admin публічну властивість accessLevel,
+//     значення якої буде передаватися під час виклику конструктора.
+
+// Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
+
+// Оголошений клас Admin
+// Клас Admin наслідує від класу User
+// Клас Admin містить публічну статичну властивість AccessLevel
+// Клас Admin містить метод constructor з параметром у вигляді об'єкта {email, accessLevel}
+// В класі Admin в конструкторі для властивості email використовується звернення до конструктора батьківського класу
+// Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+// Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//   }
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// //===============================================================
+// 20
+
+// Додай класу Admin наступні властивості і методи.
+
+// Публічну властивість blacklistedEmails для зберігання чорного списку поштових адрес користувачів.
+// Значення за замовчуванням — це порожній масив.
+// Публічний метод blacklist(email) для додавання пошти у чорний список.
+// Метод повинен додавати значення параметра email в масив, що зберігається у властивості blacklistedEmails.
+// Публічний метод isBlacklisted(email) для перевірки пошти у чорному списку.
+// Метод повинен перевіряти наявність значення параметра email в масиві, що зберігається у властивості
+// blacklistedEmails, і повертати true або false.
+// Після оголошення класу ми додали ініціалізацію екземпляра і виклики методів у тій послідовності,
+// в якій твій код перевірятимуть тести.Будь ласка, нічого там не змінюй.
+
+// Оголошений клас Admin
+// Клас Admin наслідує від класу User
+// Клас Admin містить публічну властивість blacklistedEmails
+// Клас Admin містить публічний метод blacklist
+// Клас Admin містить публічний метод isBlacklisted
+// Після виклику mango.blacklist("poly@mail.com") значення властивості blacklistedEmails - це масив ["poly@mail.com"]
+// Виклик mango.isBlacklisted("mango@mail.com") повертає false
+// Виклик mango.isBlacklisted("poly@mail.com") повертає true
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Change code below this line
+//   blacklistedEmails = [];
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//   }
+
+//   blacklist(email) {
+//     this.blacklistedEmails.push(email);
+//   }
+
+//   isBlacklisted(email) {
+//     return this.blacklistedEmails.includes(email);
+//   }
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist('poly@mail.com');
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted('mango@mail.com')); // false
+// console.log(mango.isBlacklisted('poly@mail.com')); // true
+
+// //===============================================================
