4de3101df43707288abcd5e0960144029e6d68be
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-16 01:10:40 +0200

    update files 1-9 to 6 module 11 lesson

--- js/repeta/11-DOM/01-query-selector.js
+++ js/repeta/11-DOM/01-query-selector.js
@@ -9,7 +9,9 @@ console.log(magicBtn);
 const navEl = document.querySelector('.site-nav');
 console.log(navEl);
 
-const navLinksEl = document.querySelectorAll('.site-nav__link');
-console.log('navLinksEl', navLinksEl);
+magicBtn.addEventListener('click', () => {
+  const navLinksEl = document.querySelectorAll('.site-nav__link');
+  console.log('navLinksEl', navLinksEl);
+});
 
 // // Document.querySelector* и Element.querySelector*
