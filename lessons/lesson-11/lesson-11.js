26c397a10fe4a5f8fa65d542a897bc7f46b86e32
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-20 23:52:56 +0200

    update 2task from 11 lesson (DOM)

--- lessons/lesson-11/lesson-11.js
+++ lessons/lesson-11/lesson-11.js
@@ -20,38 +20,140 @@ const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
 </ul>
 */
 
-const listEl = document.createElement('ul');
-// console.log(listEl);
+// //----------- for of ------------------
 
-for (const technology of technologies) {
-  const liEl = document.createElement('li');
+// const listEl = document.createElement('ul');
+// // console.log(listEl);
 
-  // // 1. var
-  // liEl.textContent = technology;
-  // // 2. var
-  const text = document.createTextNode(technology);
-  liEl.appendChild(text);
+// for (const technology of technologies) {
+//   const liEl = document.createElement('li');
 
-  listEl.appendChild(liEl);
-}
+//   // // 1. var
+//   // liEl.textContent = technology;
+//   // // 2. var
+//   const text = document.createTextNode(technology);
+//   liEl.appendChild(text);
 
-// console.log(listEl);
+//   listEl.appendChild(liEl);
+// }
 
-// додає в самий кінець
-// document.body.appendChild(listEl);
+// // console.log(listEl);
 
-// додає у відповідний вузол
+// // додає в самий кінець
+// // document.body.appendChild(listEl);
 
-const hero = document.querySelector('.hero');
-hero.after(listEl);
+// // додає у відповідний вузол
+// const hero = document.querySelector('.hero');
+// hero.after(listEl);
 
 // element.append(el1, el2, ...) - додає один або декілька елементів після всіх дітей елемента element.
 // element.prepend(el1, el2, ...) - додає один або декілька елементів перед усіма дітьми елемента element.
 // element.after(el1, el2, ...) - додає один або декілька елементів після елемента element.
 // element.before(el1, el2, ...) - додає один або декілька елементів перед елементом element.
 
+// // ------- map --------------
+
+// const markup = `<ul>${technologies
+//   .map(technology => `<li>${technology}</li>`)
+//   .join('')}</ul>`;
+// // console.log(markup);
+
+// додає в самий кінець
+// document.body.insertAdjacentHTML('beforeend', markup);
+
+// // додає у відповідний вузол
+// const hero = document.querySelector('.hero');
+// hero.insertAdjacentHTML('afterend', markup);
+
+// //---------- reduce ------------
+
+const markup = `<ul>${technologies.reduce(
+  (acc, technology) => (acc += `<li>${technology}</li>`),
+  ''
+)}</ul>`;
+// // console.log(markup);
+
+// // додає у відповідний вузол
+const hero = document.querySelector('.hero');
+hero.insertAdjacentHTML('afterend', markup);
+
 // ==========================================================================
 
 //* 2. Створити кілька кнопок на основі масива 3 обектами використовуючи createElement
 
-console.log(document);
+const colors = [
+  {
+    label: 'red',
+    color: '#FF0000',
+  },
+  {
+    label: 'green',
+    color: '#00FF00',
+  },
+  {
+    label: 'blue',
+    color: '#0000FF',
+  },
+  {
+    label: 'yellow',
+    color: '#FFFF00',
+  },
+];
+
+/*
+<div>
+    <button type='button' stye='someStyles'>red</button>
+    <button type='button' stye='someStyles'>green</button>
+    <button  type='button' stye='someStyles'>blue</button>
+</div>
+*/
+
+// //----------- for of ------------------
+
+const btnWrap = document.createElement('div');
+btnWrap.style.display = 'flex';
+btnWrap.style.alignItems = 'center';
+btnWrap.style.justifyContent = 'center';
+btnWrap.style.marginBottom = '1rem';
+// btnWrap.style.flexDirection = 'column';
+// btnWrap.style.alignContent = 'center';
+console.log(btnWrap);
+
+for (const { label, color } of colors) {
+  const btn = document.createElement('button');
+  btn.textContent = label;
+  btn.type = 'button';
+  btn.style.backgroundColor = color;
+  btn.className = 'magic-btn';
+
+  btnWrap.appendChild(btn);
+}
+
+// додає у відповідний вузол
+// const hero = document.querySelector('.hero');
+hero.after(btnWrap);
+
+// // ------- map --------------
+
+const btnWrapMap = document.createElement('div');
+btnWrapMap.style.display = 'flex';
+// btnWrapMap.style.alignItems = 'center';
+btnWrapMap.style.justifyContent = 'center';
+btnWrap.style.marginBottom = '1rem';
+// btnWrapMap.style.flexDirection = 'column';
+// btnWrapMap.style.alignContent = 'center';
+console.log(btnWrapMap);
+
+colors.map(({ label, color }) => {
+  const btn = document.createElement('button');
+  btn.textContent = label;
+  btn.type = 'button';
+  btn.style.backgroundColor = color;
+  btn.className = 'back-btn';
+
+  btnWrapMap.appendChild(btn);
+});
+
+btnWrap.after(btnWrapMap);
+
+// //---------- reduce ------------
