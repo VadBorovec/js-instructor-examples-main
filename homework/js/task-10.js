1f252c1191d5934c3196f97bdf1149991dcdd7f0
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-22 04:17:51 +0200

    update task-10 in homework

--- homework/js/task-10.js
+++ homework/js/task-10.js
@@ -58,3 +58,38 @@ function createBoxes(amount) {
 function destroyBoxes() {
   boxes.innerHTML = '';
 }
+
+// // ========================================================================================================
+
+
+// const createBtn = document.querySelector('[data-create]');
+// const destroyBtn = document.querySelector('[data-destroy]');
+// const boxesDiv = document.querySelector('#boxes');
+
+// createBtn.addEventListener('click', createBoxes);
+// destroyBtn.addEventListener('click', destroyBoxes);
+
+// function createBoxes() {
+//   const amount = Number(document.querySelector('input').value);
+
+//   let boxes = '';
+//   let size = 30;
+
+//   for (let i = 0; i < amount; i++) {
+//     const color = getRandomHexColor();
+//     boxes += `<div style="width: ${size}px; height: ${size}px; background-color: ${color};"></div>`;
+//     size += 10;
+//   }
+
+//   boxesDiv.innerHTML = boxes;
+// }
+
+// function destroyBoxes() {
+//   boxesDiv.innerHTML = '';
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
